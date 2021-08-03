6b0f5d168d66ad01a78532016b13940bc5ab1cc8
IsseiNagi <issei.nagi@gmail.com>
2021-08-03 16:26:25 +0900

    モデルフォームで画像込みのアップロード機能を追加

---
+++ FormApp/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Students
+from django.core import validators
+
+
+def check_grade(value):
+    if not 0 < value < 4:
+        raise validators.ValidationError('無効な入力です')
+
+
+class StudentsModelForm(forms.ModelForm):
+    name = forms.CharField(
+        label='名前',
+        max_length=10,
+        min_length=3,
+        widget=forms.TextInput(attrs={
+            'placeholder': '名前を入力してください'
+        })
+    )
+    age = forms.IntegerField(
+        label='年齢',
+        widget=forms.TextInput(attrs={
+            'placeholder': '年齢を入力してください'
+        })
+    )
+    grade = forms.IntegerField(
+        label='学年',
+        validators=[check_grade]
+        )
+    picture = forms.FileField(label='本人写真')
+
+    class Meta:
+        model = Students
+        fields = '__all__'
--- FormApp/views.py
+++ FormApp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
 
 
 def setform(request):
-    pass
+    form = forms.StudentsModelForm()
+    if request.method == 'POST':
+        form = forms.StudentsModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    return render(request, 'form.html', context={
+        'form': form
+    })
