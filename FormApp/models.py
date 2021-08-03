e752278357b6bbb86d2c5d8679c18498b3c49e34
IsseiNagi <issei.nagi@gmail.com>
2021-08-03 13:00:46 +0900

    モデルの作成

--- FormApp/models.py
+++ FormApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Students(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    grade = models.IntegerField()
+    picture = models.FileField(upload_to='picture/%Y/%m/%d')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'students'
